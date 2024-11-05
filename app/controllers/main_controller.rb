6afd62375cce83b8ef880d8fd5d4d86250686aad
Eric Halverson <ericleifhalverson@gmail.com>
2024-11-05 15:29:41 -0800

    Rubocop linting

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,7 +6,7 @@ class MainController < ApplicationController
   end
 
   def show_post
-    post = find_post_by_id(params['id'])
+    post = find_post_by_id(params["id"])
 
     render "main/show_post", locals: { post: post }
   end
@@ -15,10 +15,10 @@ class MainController < ApplicationController
   end
 
   def edit_post
-    post = find_post_by_id(params['id'])
+    post = find_post_by_id(params["id"])
 
     render "main/edit_post", locals: { post: post }
-  end 
+  end
 
   def update_post
     update_query = <<-SQL
@@ -28,41 +28,40 @@ class MainController < ApplicationController
           author     = ?
       WHERE posts.id = ?
     SQL
-    
-    connection.execute update_query,
-      [params['title'],
-      params['body'],
-      params['author'],
-      params['id']]
 
-    redirect_to action: 'list_posts'
+    connection.execute update_query,
+      [ params["title"],
+      params["body"],
+      params["author"],
+      params["id"] ]
+
+    redirect_to action: "list_posts"
   end
 
   def create_post
-    post = Post.new('title' => params['title'],
-                    'body' => params['body'],
-                    'author' => params['author'])
+    post = Post.new("title" => params["title"],
+                    "body" => params["body"],
+                    "author" => params["author"])
     post.save
-    redirect_to action: 'list_posts'
+    redirect_to action: "list_posts"
   end
 
   def delete_post
-    connection.execute("DELETE FROM posts WHERE posts.id = ?", params['id'])
+    connection.execute("DELETE FROM posts WHERE posts.id = ?", params["id"])
 
-    redirect_to action: 'list_posts'
+    redirect_to action: "list_posts"
   end
 
   private
 
   def connection
-    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection = SQLite3::Database.new "db/development.sqlite3"
     db_connection.results_as_hash = true
     db_connection
   end
 
   def find_post_by_id(id)
-    connection.execute("SELECT * FROM posts 
+    connection.execute("SELECT * FROM posts
     WHERE posts.id = ? LIMIT 1", id).first
   end
-   
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,12 @@
 class Post
   attr_reader :id, :title, :body, :author, :created_at
 
-  def initialize(attributes={})
-    @id = attributes['id']
-    @title = attributes['title']
-    @body = attributes['body']
-    @author = attributes['author']
-    @created_at = attributes['created_at']
+  def initialize(attributes = {})
+    @id = attributes["id"]
+    @title = attributes["title"]
+    @body = attributes["body"]
+    @author = attributes["author"]
+    @created_at = attributes["created_at"]
   end
 
   def save
@@ -25,9 +25,8 @@ class Post
   end
 
   def connection
-    db_connection = SQLite3::Database.new 'db/development.sqlite3'
+    db_connection = SQLite3::Database.new "db/development.sqlite3"
     db_connection.results_as_hash = true
     db_connection
   end
-
 end
